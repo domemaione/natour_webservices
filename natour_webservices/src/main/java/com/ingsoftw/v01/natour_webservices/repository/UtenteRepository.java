a7eee46ecab134a68d34050209180b6279af229a
domenicomaione <domenico.maione@outlook.it>
2021-12-06 12:13:44 +0100

    Change project path root

--- natour_webservices/src/main/java/com/ingsoftw/v01/natour_webservices/repository/UtenteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ingsoftw.v01.natour_webservices.repository;
-
-import com.ingsoftw.v01.natour_webservices.model.Utente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UtenteRepository extends JpaRepository<Utente, Long> {
-
-}
